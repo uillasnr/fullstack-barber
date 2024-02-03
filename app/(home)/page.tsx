6297b4daa12011c53e1c85613c18cd979615ec9f
uillasnr <wiliasreis@hotmail.com>
2024-02-03 17:55:24 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 import { format } from "date-fns";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
@@ -13,6 +14,10 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
   );
