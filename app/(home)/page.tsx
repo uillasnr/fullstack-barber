84ad0ad3c7958cd2bf0251983401298665dc9090
uillasnr <wiliasreis@hotmail.com>
2024-02-04 16:54:57 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -44,6 +44,18 @@ export default async function Home() {
           ))}
         </div>
       </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-sm uppercase text-gray-400 font-bold mb-3">
+          Populares
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/_components/footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-secondary py-6 px-5">
+      <div className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright FSW Barber</div>
+    </footer>
+  );
+};
+export default Footer;
